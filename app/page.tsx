e1d6dd3598ff5bb0eca264cc9904a0148432c5ee
Daniel Hika <hdntandika@gmail.com>
2025-08-26 13:54:53 +0200

    Add

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Contact from "@/components/sections/contact";
 import Experience from "@/components/sections/experience";
 import Hero from "@/components/sections/hero";
 import Projects from "@/components/sections/projects";
-import Testimonials from "@/components/sections/testimonials";
+// import Testimonials from "@/components/sections/testimonials";
 import SkillsSlider from "@/components/sections/skills";
 import Stats from "@/components/sections/stats";
 import CurvedLoop from "@/animations/CurvedLoop/CurvedLoop";
